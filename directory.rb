cb3ffc7178fd86da77f52d227cc88ca09876dc51
JC Morgan <jcmorgan@mastudemaxzq6l4.home>
2022-11-03 14:00:45 +0000

    Add comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+#prints the list of students:
+
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +13,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+#prints the total number of students:
 
 print "Overall, we have "
 print 11
